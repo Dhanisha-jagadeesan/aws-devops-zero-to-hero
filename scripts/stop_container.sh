7ce9fdabc65a6cf43ec8f1bf45e2288876a1eb16
Dhanisha-jagadeesan <147296815+Dhanisha-jagadeesan@users.noreply.github.com>
2024-09-26 16:34:18 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+
+# Starting the Docker container
+docker run your-image-name || { echo "Docker run failed with exit code $?"; exit 125; }
